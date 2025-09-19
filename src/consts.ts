c469af1eb355fcb0fa4a83d742ef5b312904932b
Nizzy <uanandaraja@gmail.com>
2025-09-19 17:32:06 +0700

    edit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Nizzy Space";
+export const SITE_MAIN_TITLE = "Nizzy's Info";
+export const SITE_BLOG_TITLE = "Nizzy's Blog";
 export const SITE_DESCRIPTION = "Homepage of @nizzyhussle";
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,11 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import { SITE_BLOG_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
 	return rss({
-		title: SITE_TITLE,
+		title: SITE_BLOG_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: posts.map((post) => ({
